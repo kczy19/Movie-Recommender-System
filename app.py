e64aa2bdbbbca3c9509d51b79b7d7f239a4ba544
kamal_chhimwal <kamal.gehu2021@gmail.com>
2023-08-11 21:40:29 +0530

    changed

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ movie_dict = pickle.load(open('movie_dict.pkl','rb'))
 movies = pd.DataFrame(movie_dict)
 similarity = pickle.load(open('similarity.pkl','rb'))
 
+st.set_page_config(
+   page_title="Movie Recommender System",
+   page_icon="🎬",
+   #layout="wide",
+   initial_sidebar_state="expanded",
+)
+
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=f77c216abcdb5820bf7325320922bdda&language=en-US".format(movie_id)
     data = requests.get(url)
@@ -18,7 +25,7 @@ def fetch_poster(movie_id):
 def recommend(selected):
     movie_index = movies[movies['title'] == selected].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True,key = lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)), reverse=True,key = lambda x: x[1])[1:11]
     recommended_movies = []
     movie_poster = []
     for i in movies_list:
@@ -35,10 +42,10 @@ selected_movie = st.selectbox('Select Movie',movies['title'].values)
 if st.button('Recommend'):
     names,posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
+    col6, col7, col8, col9, col10 = st.columns(5)
     with col1:
         st.text(names[0])
         st.image(posters[0])
-
     with col2:
         st.text(names[1])
         st.image(posters[1])
@@ -49,7 +56,28 @@ if st.button('Recommend'):
     
     with col4:
         st.text(names[3])
-        st.image(posters[3])
+        st.image(posters[3], )
+    
     with col5:
         st.text(names[4])
         st.image(posters[4])
+    
+    with col6:
+        st.text(names[5])
+        st.image(posters[5])
+
+    with col7:
+        st.text(names[6])
+        st.image(posters[6])
+
+    with col8:
+        st.text(names[7])
+        st.image(posters[7])
+    
+    with col9:
+        st.text(names[8])
+        st.image(posters[8])
+    
+    with col10:
+        st.text(names[9])
+        st.image(posters[9])
